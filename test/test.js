78f8ac55febd8dcbbf4e0f6d113fdf23214dbda0
Soares Chen <quiverjs@soareschen.com>
2013-04-02 01:36:39 +0200

    First commit for quiver-file-handler.

---
+++ test/test.js
@@ -0,0 +1,228 @@
+
+var fileHandler = require('../lib/index')
+var fileStream = require('quiver-file-stream')
+var streamConvert = require('quiver-stream-convert')
+var streamChannel = require('quiver-stream-channel')
+var should = require('should')
+var async = require('async')
+var pathUtil = require('path')
+
+var testDirectory = pathUtil.join(__dirname, '../lib')
+var testFileName1 = 'file-handler.js'
+var testFileName2 = 'directory-handler.js'
+var testFile1 = pathUtil.join(testDirectory, testFileName1)
+var testFile2 = pathUtil.join(testDirectory, testFileName2)
+
+var getFileContent = function(filePath, callback) {
+  fileStream.createFileReadStream(filePath, function(err, readStream) {
+    if(err) throw err
+
+    streamConvert.streamToText(readStream, function(err, text) {
+      if(err) throw err
+
+      callback(text)    
+    })
+  })
+}
+
+var assertSameContent = function(originalContent, readStream, callback) {
+  streamConvert.streamToText(readStream, function(err, text) {
+    if(err) throw err
+
+    text.should.equal(originalContent)
+    callback()
+  })
+}
+
+var assertHandlerCreatesContent = function(originalContent, handler, args, callback) {
+  handler(args, streamChannel.createEmptyStreamable(),
+    function(err, resultStreamable) {
+      if(err) throw err
+
+      assertSameContent(originalContent, resultStreamable.toStream(), callback)
+    })
+}
+
+describe('file handler test', function() {
+  it('should return same content as reading file directly', function(callback) {
+    getFileContent(testFile1, function(originalContent) {
+      fileHandler.createFileStreamHandler({
+        filePath: testFile1
+      }, function(err, handler) {
+        if(err) throw err
+
+        assertHandlerCreatesContent(originalContent, handler, 
+          { path: 'ignored' },
+          function() {
+            assertHandlerCreatesContent(originalContent, handler, 
+              { path: 'still the same cotent' }, callback)
+          })
+      })
+    })
+  })
+})
+
+describe('directory handler test', function() {
+  it('should return different files content based on path', function(callback) {
+    getFileContent(testFile1, function(content1) {
+      getFileContent(testFile2, function(content2) {
+        
+        fileHandler.createFileDirectoryHandler({ dirPath: testDirectory }, 
+          function(err, handler) {
+
+            assertHandlerCreatesContent(content1, handler, 
+              { path: '/' + testFileName1 }, function() {
+
+                assertHandlerCreatesContent(content2, handler, 
+                  { path: '/' + testFileName2 }, callback)
+              })
+          })
+      })
+    })
+  })
+
+  it('should return error 404 for non-existing files', function(callback) {
+    fileHandler.createFileDirectoryHandler({ dirPath: testDirectory }, 
+      function(err, handler) {
+        handler({ path: '/non-existing-file' }, streamChannel.createEmptyStreamable(),
+          function(err, resultStreamable) {
+            should.exist(err)
+            should.not.exist(resultStreamable)
+
+            err.errorCode.should.equal(404)
+            callback(null)
+          })
+      })
+  })
+})
+
+describe('test valid path filter', function() {
+
+  it('should reject invalid paths', function(callback) {
+    var mockHandler = function() {
+      throw new Error('should never called')
+    }
+
+    fileHandler.createValidPathFilter({ }, mockHandler, function(err, handler) {
+      if(err) throw err
+
+      var invalidPaths = ['./current', '../parent', '/child/../parent', '/two//slashes']
+      async.each(invalidPaths, function(path, callback) {
+        var args = { path: path }
+        handler(args, streamChannel.createEmptyStreamable(), function(err) {
+          err.errorCode.should.equal(400)
+          callback(null)
+        })
+      }, callback)
+    })
+  })
+
+  it('should accept valid paths', function(callback) {
+    var mockHandler = function(args, inputStreamable, callback) {
+      callback(null, streamChannel.createEmptyStreamable())
+    }
+
+    fileHandler.createValidPathFilter({ }, mockHandler, function(err, handler) {
+      if(err) throw err
+
+      var validPaths = ['/file', 'name', 'dir/', '/dir/']
+      async.each(validPaths, function(path, callback) {
+        var args = { path: path }
+        handler(args, streamChannel.createEmptyStreamable(), function(err, resultStreamable) {
+          should.not.exist(err)
+          should.exist(resultStreamable)
+          callback(null)
+        })
+      }, callback)
+    })
+  })
+
+})
+
+describe('test index path filter', function(callback) {
+  it('should found index file in directory', function(callback) {
+    var mockHandler = function(args, inputStreamable, callback) {
+      callback(null, streamConvert.textToStreamable(args.path))
+    }
+
+    var indexNames = ['index.js']
+    var config = {
+      dirPath: testDirectory,
+      indexNames: indexNames
+    }
+
+    fileHandler.createIndexPathFilter(config, mockHandler, function(err, handler) {
+      if(err) throw err
+
+      var args = {
+        path: '/'
+      }
+
+      handler(args, streamChannel.createEmptyStreamable(), function(err, resultStreamable) {
+        if(err) throw err
+
+        streamConvert.streamableToText(resultStreamable, function(err, text) {
+          if(err) throw err
+
+          text.should.equal('/index.js')
+          callback(null)
+        })
+      })
+    })
+  })
+
+  it('should fail to find index.html', function(callback) {
+    var mockHandler = function() {
+      throw new Error('should never called')
+    }
+
+    var indexNames = ['index.html']
+    var config = {
+      dirPath: testDirectory,
+      indexNames: indexNames
+    }
+
+    fileHandler.createIndexPathFilter(config, mockHandler, function(err, handler) {
+      if(err) throw err
+
+      var args = { path: '/' }
+
+      handler(args, streamChannel.createEmptyStreamable(), function(err, resultStreamable) {
+        should.not.exist(resultStreamable)
+        err.errorCode.should.equal(404)
+        callback(null)
+      })
+    })
+  })
+})
+
+describe('test content type filter', function(callback) {
+  it('should detect and add content type to result streamable', function(callback) {
+    var mockHandler = function(args, inputStreamable, callback) {
+      callback(null, streamChannel.createEmptyStreamable())
+    }
+
+    fileHandler.createContentTypeFilter({ }, mockHandler, function(err, handler) {
+      if(err) throw err
+
+      var testTypes = [
+        { path: '/a/file.txt', contentType: 'text/plain' },
+        { path: '/index.html', contentType: 'text/html' },
+        { path: '/path/to/image.jpg', contentType: 'image/jpeg'}
+      ]
+
+      async.each(testTypes, function(testType, callback) {
+        var args = { path: testType.path }
+
+        handler(args, streamChannel.createEmptyStreamable(), function(err, resultStreamable) {
+          should.not.exist(err)
+          should.exist(resultStreamable.contentType)
+          resultStreamable.contentType.should.equal(testType.contentType)
+
+          callback(null)
+        })
+      }, callback)
+    })
+  })
+})
+
